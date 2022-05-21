7c2a63a5873b7237bd793cac86e88af0f0c946a6
PruebasPA <jandpos@cqteduca.edu.com>
2022-05-21 11:53:33 -0500

    cambios

--- Tienda2.0/js/tiendasi.js
+++ Tienda2.0/js/tiendasi.js
@@ -6,4 +6,16 @@ document.getElementById('formulario')
      const precio= document.getElementById('precio').value;
      const categoria= document.getElementById('categoria').value;
      e.preventDefault();
-    })
+    });
+const listapro=document.getElementById('listapro');
+const element= document.createElement('div');
+element.innerHTML= `
+<div class="card text-center mb-4">
+<div class="card-body">
+<strong>Nombre</strong>: ${nombre}
+<strong>precio</strong>: ${precio}
+<strong>categoria</strong>: ${categoria}
+</div>
+</div>
+`;
+listapro.appendChild(element);
